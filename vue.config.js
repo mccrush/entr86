f178d1d95fdd7f56257d872d8f8744b312fcfbec
mccrush <mccrush2027@gmail.com>
2020-06-09 12:54:35 +0700

    Add vue config

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  productionSourceMap: false,
+
+  pwa: {
+    name: 'Enter',
+    themeColor: '#F5A623',
+    manifestOptions: {
+      background_color: '#F5A623'
+    }
+  }
+}
