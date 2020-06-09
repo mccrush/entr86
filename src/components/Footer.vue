b9f5b0af0885c8fb9d1ceac6448fcd1f30613d34
mccrush <mccrush2027@gmail.com>
2020-06-09 13:41:40 +0700

    Config Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,3 +1,7 @@
 <template>
-  <h4>Footer</h4>
+  <div class="row bg-dark text-light text-left">
+    <div class="col-4">Contacts</div>
+    <div class="col-4"></div>
+    <div class="col-4">Clients</div>
+  </div>
 </template>
