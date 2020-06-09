55dd3f3f1517c7c1ae6fcbe2291d1c8bccef1346
mccrush <mccrush2027@gmail.com>
2020-06-09 17:47:58 +0700

    Add Contat info

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,12 +2,20 @@
   <div class="bg-dark">
     <div class="container">
       <div class="row text-light text-left">
-        <div class="col-12 col-sm-6 col-md-4">
-          Contacts
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero magni atque reprehenderit at illo nobis quod maiores ut impedit officiis, magnam natus dignissimos, aliquid adipisci eaque odio cum? Odio, rerum!</p>
+        <div class="col-12 col-sm-6 col-md-4 pt-3 pb-3">
+          <h5>Контакты</h5>
+          <p class="mb-2">
+            ООО ЭНТР 628415,
+            <br />Тюменская обл. ХМАО-Югра г. Сургут,
+            <br />ул. Инжинерная 1/4
+            <br />+7 (3462) 630 475
+            <br />entr86@mail.ru
+          </p>
+          <img src="img/footer/inst3.png" class="mr-2" alt="Instagram" width="40" height="40" />
+          <img src="img/footer/vk.png" alt="Vk" width="40" height="40" />
         </div>
         <div class="d-none d-md-block col-md-4"></div>
-        <div class="col-12 col-sm-6 col-md-4">
+        <div class="col-12 col-sm-6 col-md-4 pt-3">
           Clients
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero magni atque reprehenderit at illo nobis quod maiores ut impedit officiis, magnam natus dignissimos, aliquid adipisci eaque odio cum? Odio, rerum!</p>
         </div>
