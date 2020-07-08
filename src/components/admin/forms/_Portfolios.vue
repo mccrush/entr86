a993053ba50a6adaf2de2067adef2772db9d4921
mccrush <mccrush2027@gmail.com>
2020-07-08 11:46:33 +0700

    Add form Portfolio

--- src/components/admin/forms/_Portfolios.vue
+++ src/components/admin/forms/_Portfolios.vue
@@ -10,55 +10,14 @@
           required
         />
       </div>
-      <div class="col-6"></div>
-    </div>
-    <div class="row mt-3">
       <div class="col-6">
-        <div class="form-group row">
-          <label for="position" class="col-4 pt-1">Позиция</label>
-          <div class="col-4">
-            <input
-              type="number"
-              id="position"
-              min="0"
-              max="42"
-              step="1"
-              v-model="position"
-              class="form-control form-control-sm"
-            />
-          </div>
-          <div class="col-4 pt-1">
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" v-model="active" id="active" />
-              <label class="form-check-label" for="active">Активна</label>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-6 text-left"></div>
-    </div>
-    <div class="row">
-      <div class="col-12">
-        <textarea
+        <input
           type="text"
-          v-model="desc"
+          v-model="alias"
           class="form-control form-control-sm"
-          placeholder="Описание"
-          rows="9"
-        ></textarea>
-      </div>
-    </div>
-    <div class="row mt-3">
-      <div class="col-6 text-left">
-        <form>
-          <div class="form-group">
-            <label for="img">Изображение</label>
-            <input type="file" class="form-control-file" id="img" />
-          </div>
-        </form>
-      </div>
-      <div class="col-6">
-        <img src="@/assets/icons/image.svg" alt="Изображение" class height="80" />
+          placeholder="Алиас"
+          required
+        />
       </div>
     </div>
     <div class="row mt-3">
@@ -88,23 +47,17 @@ export default {
   data() {
     return {
       title: this.doc.title || '',
-      desc: this.doc.desc || '',
-      position: +this.doc.position || this.length + 1,
-      active: this.doc.active || true,
-      img: this.doc.img || ''
+      alias: this.doc.alias || ''
     }
   },
   methods: {
     async saveDoc() {
       let doc = {}
-      if (this.title.trim()) {
+      if (this.title.trim() && this.alias.trim()) {
         doc = {
           id: this.doc.id || Date.now().toString(),
           title: this.title.trim(),
-          desc: this.desc,
-          position: +this.position,
-          active: this.active,
-          img: this.img || ''
+          alias: this.alias.trim()
         }
         if (this.doc.id) {
           try {
@@ -125,10 +78,9 @@ export default {
           } catch (err) {
           } finally {
             this.title = ''
-            this.desc = ''
+            this.alias = ''
             this.position = +this.position + 1
             this.active = true
-            this.img = ''
           }
         }
       } else {
@@ -153,10 +105,7 @@ export default {
   watch: {
     doc() {
       this.title = this.doc.title
-      this.desc = this.doc.desc
-      this.position = +this.doc.position || this.length + 1
-      this.active = this.doc.active || true
-      this.img = this.doc.img
+      this.alias = this.doc.alias
     }
   }
 }
