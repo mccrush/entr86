cd69c143efe7d27e08cfdbbcb5eef92e556a1a11
mccrush <mccrush2027@gmail.com>
2020-07-15 11:38:19 +0700

    Add woed wrap

--- src/components/index/Napravs.vue
+++ src/components/index/Napravs.vue
@@ -4,29 +4,25 @@
       tag="a"
       v-for="naprav in napravs"
       :key="'np'+naprav.id"
-      :to="{path: '/price', hash: '#'+naprav.type}"
+      :to="{path: '/price', hash: '#'+naprav.alias}"
       class="col-12 col-sm-6 col-md-4 d-flex align-items-center justify-content-start mt-4 napravLink"
     >
-      <img
-        :src="'/img/index/icons/'+naprav.icon"
-        class="float-left mr-3"
-        width="76"
-        height="76"
-        alt
-      />
-      <h5 class="text-left" v-html="naprav.title"></h5>
+      <img :src="naprav.img.url" class="float-left mr-3" width="76" height="76" alt />
+      <h6 class="text-left wraping" v-html="naprav.title"></h6>
     </router-link>
   </div>
 </template>
 
 <script>
-import napravs from '@/data/napravs'
+//import napravs from '@/data/napravs'
+
 export default {
-  data() {
-    return {
-      napravs
-    }
-  }
+  props: ['napravs']
+  // data() {
+  //   return {
+  //     napravs
+  //   }
+  // }
 }
 </script>
 
@@ -41,5 +37,9 @@ export default {
 
 .napravLink:hover {
   top: -4px;
+}
+
+.wraping {
+  word-break: break-word;
 }
 </style>
