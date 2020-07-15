9660f618e7eacbbac669a874d96f5317251eb457
mccrush <mccrush2027@gmail.com>
2020-07-15 11:39:37 +0700

    Add v-html prop

--- src/components/admin/Docs.vue
+++ src/components/admin/Docs.vue
@@ -3,7 +3,8 @@
     class="btn btn-sm btn-block rounded-0 text-left m-0"
     :class="selectDocId === doc.id ? 'btn-warning' : 'btn-light'"
     @click="$emit('select-doc', doc.id)"
-  >{{doc.title}}</button>
+    v-html="doc.title"
+  ></button>
 </template>
 
 <script>
