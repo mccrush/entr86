1f6030982b34b182c602ad0dbaef242d695a2d1a
mccrush <mccrush2027@gmail.com>
2020-12-29 17:13:23 +0700

    Update 404 in route

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,14 @@
 //import Vue from 'vue'
 //import Vuex from 'vuex'
 
-import { creareStore } from 'vuex'
+import { createStore } from 'vuex'
 import data from './modules/data'
 import user from './modules/user'
 import message from './modules/message'
 
 //Vue.use(Vuex)
 
-export default creareStore({
+export default createStore({
   modules: {
     data,
     user,
