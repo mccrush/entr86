993e8da1699df23b6b4a2a8d9a57af118e1d70ee
mccrush <mccrush2027@gmail.com>
2020-12-29 17:02:17 +0700

    Update vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,14 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+//import Vue from 'vue'
+//import Vuex from 'vuex'
+
+import { creareStore } from 'vuex'
 import data from './modules/data'
 import user from './modules/user'
 import message from './modules/message'
 
-Vue.use(Vuex)
+//Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+export default creareStore({
   modules: {
     data,
     user,
