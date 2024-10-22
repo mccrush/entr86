e00d7962a40731033a7fd8765ee824703c89061a
mccrush <mccrush2027@gmail.com>
2024-10-22 14:34:43 +0700

    Update firebase API

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,28 @@
-import firebase from "firebase/app"
-import "firebase/auth"
-import "firebase/firestore" // DB
-import "firebase/storage" // File
-import apiKey from '../apiKey'
+// import firebase from "firebase/app"
+// import "firebase/auth"
+// import "firebase/firestore"
+// import "firebase/storage"
+// import apiKey from '../apiKey'
+
+// const firebaseConfig = {
+//   apiKey,
+//   authDomain: "enter-e221d.firebaseapp.com",
+//   databaseURL: "https://enter-e221d.firebaseio.com",
+//   projectId: "enter-e221d",
+//   storageBucket: "enter-e221d.appspot.com",
+//   messagingSenderId: "623507782585",
+//   appId: "1:623507782585:web:8e325a582c84cd47ffd468",
+//   measurementId: "G-EG4N6S9PK4"
+// };
+
+// firebase.initializeApp(firebaseConfig);
+// export const db = firebase.firestore();
+// export const auth = firebase.auth();
+// export const storage = firebase.storage();
+
+//
+import { initializeApp } from 'firebase/app'
+import apiKey from './../apiKey'
 
 const firebaseConfig = {
   apiKey,
@@ -15,7 +35,6 @@ const firebaseConfig = {
   measurementId: "G-EG4N6S9PK4"
 };
 
-firebase.initializeApp(firebaseConfig);
-export const db = firebase.firestore();
-export const auth = firebase.auth();
-export const storage = firebase.storage();
+const fireApp = initializeApp(firebaseConfig)
+
+export default fireApp
