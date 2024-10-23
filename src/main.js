8f13303c175ebfd7d0082280e624c65463a49e85
mccrush <mccrush2027@gmail.com>
2024-10-23 18:24:33 +0700

    Hide comment

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@ console.log('main.js: App is run')
 
 // События хуков авторизации
 const { data } = supabase.auth.onAuthStateChange((event, session) => {
-  console.log(event, session)
+  //console.log(event, session)
 
   if (event === 'INITIAL_SESSION') {
     // handle initial session
@@ -31,6 +31,7 @@ const { data } = supabase.auth.onAuthStateChange((event, session) => {
     // handle sign out event
     store.commit('setAuthData', { type: 'userId', data: null })
     store.commit('setAuthData', { type: 'userEmail', data: null })
+    router.push('/login')
   } else if (event === 'PASSWORD_RECOVERY') {
     // handle password recovery event
   } else if (event === 'TOKEN_REFRESHED') {
